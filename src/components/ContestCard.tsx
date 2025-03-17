f721df8792d78c186b9137824d3535ff6e0efd1f
Akshat Sipany <borncancer21@gmail.com>
2025-03-17 10:34:36 +0530

    contest cards ui ready

--- src/components/ContestCard.tsx
+++ src/components/ContestCard.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import {
   Card,
   CardHeader,
@@ -6,6 +7,9 @@ import {
   CardDescription,
   CardContent,
 } from "./ui/card";
+import { Calendar, Clock } from "lucide-react";
+import { Button } from "./ui/button";
+import { ExternalLink } from "lucide-react";
 interface Contest {
   id: string;
   platform: string;
@@ -57,19 +61,66 @@ export default function ContestCard() {
         return "bg-gray-200 text-gray-700";
     }
   };
+  const checkStatus = (status: string) => {
+    switch (status) {
+      case "upcoming":
+        return "bg-upcoming-secondary text-upcoming-primary";
+      case "completed":
+        return "bg-completed-secondary text-completed-primary";
+      case "ongoing":
+        return "bg-ongoing-secondary text-ongoing-primary";
+      default:
+        return "bg-gray-200 text-gray-700";
+    }
+  };
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 gap-x-5.5 gap-y-5.5 px-4">
       {dummyContests.map((contest: Contest) => {
         return (
-          <Card key={contest.id}>
-            <CardHeader className="flex justify-between items-center">
-              <div
-                className={`${checkPlatform(contest.platform.toLowerCase())}`}
-              >
-                {contest.platform}
+          <Card key={contest.id} className="flex flex-col items-start w-full">
+            <CardHeader className="flex flex-col w-full">
+              <div className="flex justify-between items-center w-full">
+                <div
+                  className={cn(
+                    `${checkPlatform(contest.platform.toLowerCase())}`,
+                    "rounded-sm px-2 py-1 text-sm font-medium"
+                  )}
+                >
+                  {contest.platform}
+                </div>
+                <div
+                  className={cn(
+                    `${checkStatus(contest.status.toLowerCase())}`,
+                    "rounded-sm px-2 py-1 text-sm font-medium uppercase"
+                  )}
+                >
+                  {contest.status}
+                </div>
               </div>
-              <div>{contest.status}</div>
+              <CardTitle className="font-medium mt-2">{contest.name}</CardTitle>
+              <CardDescription className="mt-2 flex flex-col gap-y-2">
+                <div className="flex items-center gap-2">
+                  <div>
+                    <Calendar size={16} />
+                  </div>
+                  <div>{contest.startTime}</div>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div>
+                    <Clock size={16} />
+                  </div>
+                  <div>{contest.duration}</div>
+                </div>
+              </CardDescription>
             </CardHeader>
+            <CardFooter className="w-full">
+              <Button variant={"outline"} className="w-full cursor-pointer">
+                Visit {contest.platform}
+                <span>
+                  <ExternalLink />
+                </span>
+              </Button>
+            </CardFooter>
           </Card>
         );
       })}
